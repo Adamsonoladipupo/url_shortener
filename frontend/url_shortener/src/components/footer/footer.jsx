6379ce9f904083e62845eb0b784e522740a90234
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-09 21:01:45 +0100

    added footer jsx

---
+++ frontend/url_shortener/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () =>{
+    return(
+        <>
+            <div>
+                <span>
+                    url_shortener @ Copyright 2025
+                </span>
+                <span>Terms of use</span>
+                <span>Social icons</span>
+            </div>
+        </>
+    )
+}
+export default Footer
