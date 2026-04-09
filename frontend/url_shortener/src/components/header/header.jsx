914897d1fbfb6d5b60e8342ccdac5cb24450f9b9
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-09 21:03:00 +0100

    added header

---
+++ frontend/url_shortener/src/components/header/header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+            <div>
+                <span>url_shortener</span>
+                <ul>
+                    <li>Platform</li>
+                    <li>Solutions</li>
+                    <li>Resources</li>
+                    <li>Prices</li>
+                </ul>
+                <div>
+                    <button>Log in</button>
+                    <button>Start for free</button>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Header
